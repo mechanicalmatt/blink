d94df7f22eebb7e682902c9e31bcf6289125be33
Matt Gray <matt.ta.gray@gmail.com>
2025-03-02 20:07:54 -0600

    edit duration

--- blink.c
+++ blink.c
@@ -1,19 +1,7 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
 #include "pico/stdlib.h"
 
-// Pico W devices use a GPIO on the WIFI chip for the LED,
-// so when building for Pico W, CYW43_WL_GPIO_LED_PIN will be defined
-#ifdef CYW43_WL_GPIO_LED_PIN
-#include "pico/cyw43_arch.h"
-#endif
-
 #ifndef LED_DELAY_MS
-#define LED_DELAY_MS 250
+#define LED_DELAY_MS 1000
 #endif
 
 // Perform initialisation
@@ -24,9 +12,6 @@ int pico_led_init(void) {
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
     return PICO_OK;
-#elif defined(CYW43_WL_GPIO_LED_PIN)
-    // For Pico W devices we need to initialise the driver etc
-    return cyw43_arch_init();
 #endif
 }
 
@@ -35,9 +20,6 @@ void pico_set_led(bool led_on) {
 #if defined(PICO_DEFAULT_LED_PIN)
     // Just set the GPIO on or off
     gpio_put(PICO_DEFAULT_LED_PIN, led_on);
-#elif defined(CYW43_WL_GPIO_LED_PIN)
-    // Ask the wifi "driver" to set the GPIO on or off
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
 #endif
 }
 
@@ -49,5 +31,9 @@ int main() {
         sleep_ms(LED_DELAY_MS);
         pico_set_led(false);
         sleep_ms(LED_DELAY_MS);
+        pico_set_led(true);
+        sleep_ms(100);
+        pico_set_led(false);
+        sleep_ms(200);
     }
 }
